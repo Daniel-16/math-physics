231b2018fab92d16b63dbadb4c0fe3cd065e13b8
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2023-05-08 14:20:27 +0100

    initial commit

---
+++ classes.py
@@ -0,0 +1,22 @@
+class Math:
+    def exponential(self, number, exponent):
+        pass
+    def squareRoot(self, number):
+        pass
+    def modulus(self, num1, num2):
+        pass
+    def twoDimVector(self, num1, num2):
+        pass
+    def threeDimVectors(self, num1, num2, num3):
+        pass
+class Physics:
+    def velocity(self, distance, time):
+        pass
+    def energy(self, mass):
+        pass
+    def workDone(self, force, distance):
+        pass
+    def power(self, current, volt):
+        pass
+    def force(self, mass, acceleration):
+        pass
