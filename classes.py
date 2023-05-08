20af9c997b1b2d31bb270f548d34fec55373b05b
Daniel <danieloluwasegun1000@gmail.com>
2023-05-08 14:37:30 +0100

    'Dan-funct'

--- classes.py
+++ classes.py
@@ -1,19 +1,24 @@
+import math
 class Math:
     def exponential(self, number, exponent):
         pass
     def squareRoot(self, number):
         pass
     def modulus(self, num1, num2):
-        pass
+        mod = int(num1) % int(num2)
+        return mod
     def twoDimVector(self, num1, num2):
-        pass
+        magnitude = math.sqrt(num1**2 + num2)        
+        return magnitude
     def threeDimVectors(self, num1, num2, num3):
         pass
 class Physics:
     def velocity(self, distance, time):
         pass
     def energy(self, mass):
-        pass
+        speedOfLight = 3.0 * 10**8
+        energy = int(mass) * speedOfLight
+        return energy
     def workDone(self, force, distance):
         pass
     def power(self, current, volt):
